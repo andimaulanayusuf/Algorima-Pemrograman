faa5fdca241cdf138e6b8306e7600422e53eca69
Andi Maulana Yusuf <iamandimaulana@gmail.com>
2017-04-07 08:50:01 +0700

    Tugas 6 Faktorial

---
+++ tugas fungsi faktorial.cpp
@@ -0,0 +1,10 @@
+long long faktorial(int n){
+	long long hasil ;
+	hasil = 1;
+	for(bilangan = 1; bilangan <= n;bilangan++)
+	{
+		hasil=hasil*bilangan;
+	}
+	
+	return hasil;
+}
---
+++ tugas prosedur faktorial.cpp
@@ -0,0 +1,11 @@
+void faktorial(int nilai) {
+	long hasil; 
+		hasil = 1;
+	for(bilangan = 1; bilangan <= nilai; bilangan++) {
+         hasil = hasil * bilangan;
+         
+     }
+     cout << "Hasil faktorial dari :  " << nilai << endl;
+	 cout << " Adalah : " << hasil;
+}
+
